2804ae3b62f51d11defff29e7f6d9ba2e2c62c59
CarlosLte4 <crlscastillo161@gmail.com>
2024-12-08 18:14:33 -0500

    Se agrega la ventana cursos y la de crear cursos, aun no funcionan

--- cursos/urls.py
+++ cursos/urls.py
@@ -3,4 +3,5 @@ from . import views #esto significa que el viws esta en la misma carpeta
 
 urlpatterns = [
     path('',views.cursos, name="Cursos"),
+    path('crearCurso/',views.crearCurso, name="crearCurso"),
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import Curso
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 # Create your views here.
 def cursos(request):
     
-    return render(request, "cursos/cursosP.html") #en comillas esta la capeta en la que esta el archivo html 
-    
+    return render(request, "cursos/cursosP.html") 
+
+def crearCurso(request):    
+
+    return render(request, "cursos/crearCurso.html")
