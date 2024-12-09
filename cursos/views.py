e134bc96d937d36487b6bca3a5752c559595c8f7
CarlosLte4 <crlscastillo161@gmail.com>
2024-12-08 20:34:20 -0500

    los profesores pueden crearcursos

--- cursos/views.py
+++ cursos/views.py
@@ -1,13 +1,39 @@
 from django.shortcuts import render,redirect
 from .models import Curso
-from django.contrib.auth.decorators import login_required
+from login.models import Profesor
 # Create your views here.
 
 # Create your views here.
 def cursos(request):
+    if 'profesor_id' not in request.session:
+        return redirect('login')
+    profesor_id=request.session['profesor_id']
+    try: 
+        profesor=Profesor.objects.get(id=profesor_id)
+        cursos=Curso.objects.filter(profesor=profesor.user)
+        return render(request,"cursos/cursosP.html",{'cursos':cursos})
+    except Profesor.DoesNotExist:
+        del request.session['profesor_id']
+        return redirect('login')
     
-    return render(request, "cursos/cursosP.html") 
 
-def crearCurso(request):    
+def crearCurso(request):
+    if 'profesor_id' not in request.session:
+        return redirect('login')
+    if request.method=='POST':
+        nombre=request.POST.get('nombre')
+        profesor_id=request.session['profesor_id']
+        try:
+            profesor=Profesor.objects.get(id=profesor_id)
+            if nombre:
+                Curso.objects.create(
+                    nombre=nombre,
+                    profesor=profesor.user
+                )
+                return redirect('Cursos')
+            else:
+                return render(request,'cursos/crearCurso.html',{'error':'Debe ingresar un nombrer para el curso'})
+        except Profesor.DoesNotExist:
+            return redirect('login')
+    return render(request, 'cursos/crearCurso.html')
 
-    return render(request, "cursos/crearCurso.html")
