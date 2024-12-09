c4f380780265845c44ec9c8be0f217073c61dfcc
CarlosLte4 <crlscastillo161@gmail.com>
2024-12-08 20:57:43 -0500

    Se agregan los admin de login y de cursos

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Curso
 
-# Register your models here.
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('id','nombre', 'get_profesor')
+    
+    def get_profesor(self, obj):
+        return f"{obj.profesor.first_name} {obj.profesor.last_name}"
+    get_profesor.short_description = 'Profesor'
--- login/admin.py
+++ login/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Profesor
 
-# Register your models here.
+@admin.register(Profesor)
+class ProfesorAdmin(admin.ModelAdmin):
+    list_display = ('id','get_nombre_completo', 'get_email', 'foto')
+    
+    def get_nombre_completo(self, obj):
+        return f"{obj.user.first_name} {obj.user.last_name}"
+    get_nombre_completo.short_description = 'Nombre Completo'
+    
+    def get_email(self, obj):
+        return obj.user.email
+    get_email.short_description = 'Correo Electrónico'
